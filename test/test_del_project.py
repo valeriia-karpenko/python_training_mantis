775091ebacf3fb092fd3dca632a2b54f8fa79a2f
vkarpenko <valeriia.karpenko@billing.ru>
2021-02-15 15:44:05 +0300

    Задание 25

--- test/test_del_project.py
+++ test/test_del_project.py
@@ -8,10 +8,10 @@ def test_del_project(app, db, check_ui):
     app.session.login("administrator", "root")
     if len(db.get_project_list()) == 0:
         app.project.create(Project(name="test"))
-    old_projects = db.get_project_list()
+    old_projects = app.soap.get_project_list(username="administrator", password="root")
     project = random.choice(old_projects)
     app.project.delete_project_by_id(project.id)
-    new_projects = db.get_project_list()
+    new_projects = app.soap.get_project_list(username="administrator", password="root")
     old_projects.remove(project)
     assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
     if check_ui:
